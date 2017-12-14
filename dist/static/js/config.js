f93d3db1cd43ad1ccb6df4ea7e5f89d12605dd80
Mrliurock <18672423870@163.com>
2017-12-14 15:27:16 +0800

    提交文件

---
+++ dist/static/js/config.js
@@ -0,0 +1,16 @@
+// Url配置文件
+window.proname = ''
+window.g = {
+  ctBaseURL: 'https://restapi.amap.com/v3/', //  高德地址
+  ip: 'ip', //  高德ip查询地址
+  geo: 'geocode/regeo', //  高德逆编码查询地址
+  gaodeRestKey: 'a518e7a27f2e92697dd88f06206c6d4f', //  高德key
+  fileUrl: 'https://res.elvyoo.com/', //  文件图片路径
+  dtUrl: 'https://www.elvyoo.net/titles/new/', //  地图图片路径
+  baseUrl: 'https://www.elvyoo.net/yr-app/', //  接口地址
+  proUrl: 'https://www.elvyoo.net/yr-wx/', //  项目地址
+  imgUrl: window.proname + '/static/images/', //  本地静态图片地址
+  weatherImg: window.proname + '/static/images/weather/',  //  本地天气图片地址
+  appid: 'wxca282d4371b7614d',
+  pathRoute: 'http://www.elvyoo.net/yr-pathanalysis/' //  路径分析
+}
